09c92d6294485e5781118b06f9b9523af79d8aab
angelgoitia <angelgoitia1995@gmail.com>
2020-11-16 10:20:38 -0400

    udapte version app

--- database/seeds/VersionSeeder.php
+++ database/seeds/VersionSeeder.php
@@ -14,8 +14,8 @@ class VersionSeeder extends Seeder
         \DB::table("version")->insert(
             array(
                 'id'        => 1,
-                'version'   => '1.0.0',
-                'url'       => 'wwww.google.com',
+                'version'   => '1.0.1',
+                'url'       => 'https://drive.google.com/file/d/1CMtAtfHiVajGsJ3SUjlinuqH18dWg2AX/view?usp=sharing',
                 'created_at'=> date('Y-m-d H:m:s'),
                 'updated_at'=> date('Y-m-d H:m:s'),
             )
