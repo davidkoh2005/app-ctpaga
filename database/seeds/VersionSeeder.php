ddbb3ce7ddeb3ff8d18c7f616834048e0d30dbe4
angelgoitia <angelgoitia1995@gmail.com>
2022-04-24 17:24:08 -0400

    Update VersionSeeder.php

--- database/seeds/VersionSeeder.php
+++ database/seeds/VersionSeeder.php
@@ -20,8 +20,8 @@ class VersionSeeder extends Seeder
                     'created_at'    => date('Y-m-d H:m:s'),
                     'updated_at'    => date('Y-m-d H:m:s'),
                     'app'           => env('APP_NAME'),
-                    'app_ios'       => 'https://www.apple.com/la/app-store/',
-                    'app_android'   => 'https://play.google.com/store/apps/details?id=compralotodo.appBusiness',
+                    'url_ios'       => 'https://www.apple.com/la/app-store/',
+                    'url_android'   => 'https://play.google.com/store/apps/details?id=compralotodo.appBusiness',
                 )
             );
         
@@ -34,8 +34,8 @@ class VersionSeeder extends Seeder
                     'created_at'    => date('Y-m-d H:m:s'),
                     'updated_at'    => date('Y-m-d H:m:s'),
                     'app'           => 'delivery '.env('APP_NAME'),
-                    'app_ios'       => 'https://www.apple.com/la/app-store/',
-                    'app_android'   => 'https://play.google.com/store/apps/details?id=ctlleva.ctlleva',
+                    'url_ios'       => 'https://www.apple.com/la/app-store/',
+                    'url_android'   => 'https://play.google.com/store/apps/details?id=ctlleva.ctlleva',
                 )
             );
     }
